bee63054c114c56dbe9156e147af6c48854c1358
Ali-The-Great <98874288+Ali-The-Great@users.noreply.github.com>
2022-11-15 13:29:04 +0200

    FirstCommit

---
+++ EasyLabs/EasyLabs/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using EasyLabs.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace EasyLabs.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult SignUp()
+        {
+            ViewBag.Message = "User Sign Up";
+            return View();
+        }
+    }
+}
---
+++ EasyLabs/EasyLabs/Models/UserModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EasyLabs.Models
+{
+    public class UserModel
+    {
+        public int UserId { get; set; }
+
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage ="This field is required")]
+        public string FirstName { get; set; }
+        
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "This field is required")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Email Address")]
+        [Required(ErrorMessage = "This field is required")]
+        public string Email { get; set; }
+
+        [Display(Name = "Password")]
+        [Required(ErrorMessage = "This field is required")]
+        public string Password { get; set; }
+
+        [Display(Name = "Confirm Password")]
+        [Required(ErrorMessage = "This field is required")]
+        public string ConfirmPassword { get; set; }
+       
+    }
+}
